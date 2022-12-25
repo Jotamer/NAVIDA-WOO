84cdde91dd9c3f0aa4100de1829339fc92b103b3
BevM <82781273+Pivelink1@users.noreply.github.com>
2022-12-25 15:29:25 -0500

    verga

--- example_mods/data/confronting-yourself/preload  stuff.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	precacheImage('blackscreen')
-	precacheImage('hitStatic')
-	precacheImage('characters/sonic')
-	precacheImage('characters/exe')
-	precacheImage('characters/runsonic')
-	precacheImage('characters/sonic dark')
-	
-end
--- example_mods/data/disguises/preload  stuff.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-	precacheImage('blackscreen')
-	precacheImage('hitStatic')
-	precacheImage('characters/Tails')
-	precacheImage('characters/StandExe')
-	precacheImage('characters/TailsDeath')
-	precacheImage('AI_plants')
-end
